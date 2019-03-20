7752cec0917f954b7506f649335b3d4870435fe1
brianhsu <brian.hsu0702@gmail.com>
2019-03-20 21:00:49 +0800

    TesterKotlin.kt done

--- kotlin/com/train/TesterKotlin.kt
+++ kotlin/com/train/TesterKotlin.kt
@@ -1,5 +1,24 @@
 package com.train
 
-fun main(args: Array<String>) {
+import java.util.*
 
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    println("Please enter number of tickets: ")
+    var ticket = scan.nextInt()
+    println("How many round-trip tickets: ")
+    var trip = scan.nextInt()
+    TicketKt().tickets(ticket, trip)
+}
+
+class TicketKt() {
+    fun tickets(tickets:Int, roundTrip:Int) {
+        println("Total tickets: $tickets")
+        println("Round-trip: $roundTrip")
+        val tripSingle =  (tickets - roundTrip) * 1000
+        val tripBack = (2000 * 0.9f) * roundTrip
+        val tripTotal = tripSingle + tripBack
+        println("Total: $tripTotal")
+
+    }
 }
