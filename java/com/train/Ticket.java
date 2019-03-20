446ed76f5cb218063c14256d067015af64fd4ff9
brianhsu <brian.hsu0702@gmail.com>
2019-03-20 17:50:01 +0800

    Tester.java done

---
+++ java/com/train/Ticket.java
@@ -0,0 +1,12 @@
+package com.train;
+
+public class Ticket {
+    public Ticket(int tickets, int roundTrip) {
+        System.out.println("Total tickets: " + tickets);
+        System.out.println("Round-trip: " + roundTrip);
+        int tripSingle = (tickets - roundTrip) * 1000;
+        float tripBack = (2000 * 0.9f) * roundTrip;
+        int tripTotal = (int) (tripSingle + tripBack);
+        System.out.println("Total: " + tripTotal);
+    }
+}
